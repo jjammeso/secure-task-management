c436d8ccf1bbba0f6162ef03e49ad1de922a1ac7
Sonam Jamtsho <sjjamtsho@gmail.com>
2025-09-22 22:10:26 -0400

    added entities and db config

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 //Import routes
-import taskRoutes from './routes/taskRoute.ts';
-import authRoutes from './routes/authRoutes.ts';
-import auditRoutes from './routes/auditRoutes.ts';
+import taskRoutes from './routes/taskRoute';
+import authRoutes from './routes/authRoutes';
+import auditRoutes from './routes/auditRoutes';
+import { error } from 'console';
 
 const app = express();
 
@@ -14,11 +15,25 @@ app.use(express.urlencoded({extended:true}));
 
 //Routes
 app.use('/api/tasks', taskRoutes);
-app.use('/api/auth/', authRoutes);
-app.use('/api/audit', auditRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/audit-log', auditRoutes);
+
+//Health Check
+app.get('health', (req, res) => {
+    res.json({success:true, meassage:"API is running"});
+})
+
+//Global error handler
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) =>{
+    console.error(err.stack);
+    res.status(500).json({
+        success:false,
+        error: 'Something went wrong!'
+    });
+})
 
 //404 handler
-app.use('*', (req,res)=>{
+app.use((req,res, next)=>{
     res.status(404).json({
         success:false,
         error: "Route not found"
--- apps/api/src/routes/taskRoute.ts
+++ apps/api/src/routes/taskRoute.ts
@@ -2,10 +2,25 @@ import { Router } from "express";
 
 const router = Router()
 
-router.get('/', (req, res) =>{
-    res.json({message:"successful"});
+//Create task
+router.post('/', (req,res) => {
+    res.json({message:"create tasks"})
 })
 
+//List accessible tasks
+router.get('/', (req, res) => {
+    res.json({message:"list tasks"});
+})
+
+//Edit tasks
+router.put('/:id', (req,res) => {
+    res.json({message:"edit task"})
+})
+
+//Delete tasks
+router.delete('/:id', (req, res) => {
+    res.json({messge:"delete task"})
+} )
 
 
 export default router;
