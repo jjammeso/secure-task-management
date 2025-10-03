86568a7855cfe9712f5c35a8a513e4285ed0f9b6
Sonam Jamtsho <sjjamtsho@gmail.com>
2025-10-02 23:08:10 -0400

    implemented methods to manage authentication

--- apps/dashboard/src/contexts/AuthContext.tsx
+++ apps/dashboard/src/contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { apiClient } from "@/lib/apiClient";
-import { LoginDto, User } from "@libs/data/src";
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { AuthResponse, LoginDto, User } from "@libs/data/src";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
     user: User | null;
@@ -10,7 +10,6 @@ interface AuthContextType {
     isAuthenticated: boolean;
     login: (credentials: LoginDto) => Promise<void>;
     logout: () => void;
-    refreshUser: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -25,13 +24,54 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
     const isAuthenticated = (user !== null && user !== undefined);
 
-    useEffect(()=>{
-        const initAuth = async() =>[
+    useEffect(() => {
+        const initAuth = async () => {
             const token = localStorage.getItem('auth_token');
-            if(token){
+            setIsLoading(false);
+        };
+        initAuth();
+    }, []);
 
-            }
-        ]
-    },)
+    const login = async (credential: LoginDto): Promise<void> => {
+        try {
+            setIsLoading(true);
 
+            const response = await apiClient.post<AuthResponse>('/auth/login', credential);
+
+            apiClient.setToken(response.token);
+            setUser(response.user);
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    const logout = async (): Promise<void> => {
+        localStorage.removeItem('auth_token');
+        setUser(null);
+    };
+
+    const value: AuthContextType = {
+        user,
+        isLoading,
+        isAuthenticated,
+        login,
+        logout,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = (): AuthContextType =>{
+    const context = useContext(AuthContext);
+    if(context === undefined){
+        throw new Error('useAuth must be used within an AutthProvider');
+    }
+    return context;
 }
+
